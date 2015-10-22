da3f5b76e67254e77d517a2c758d8881dd6e1e7a
muhammad-ammar <mammar@gmail.com>
2015-10-22 18:33:56 +0500

    upgrade migrations
    TNL-3658

--- splash/migrations/0001_initial.py
+++ splash/migrations/0001_initial.py
@@ -1,80 +1,35 @@
 # -*- coding: utf-8 -*-
-import datetime
-from south.db import db
-from south.v2 import SchemaMigration
-from django.db import models
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+import django.db.models.deletion
+from django.conf import settings
 
 
-class Migration(SchemaMigration):
+class Migration(migrations.Migration):
 
-    def forwards(self, orm):
-        # Adding model 'SplashConfig'
-        db.create_table('splash_splashconfig', (
-            ('id', self.gf('django.db.models.fields.AutoField')(primary_key=True)),
-            ('change_date', self.gf('django.db.models.fields.DateTimeField')(auto_now_add=True, blank=True)),
-            ('changed_by', self.gf('django.db.models.fields.related.ForeignKey')(to=orm['auth.User'], null=True, on_delete=models.PROTECT)),
-            ('enabled', self.gf('django.db.models.fields.BooleanField')(default=False)),
-            ('cookie_name', self.gf('django.db.models.fields.TextField')(default='edx_splash_screen')),
-            ('cookie_allowed_values', self.gf('django.db.models.fields.TextField')(default='seen')),
-            ('unaffected_usernames', self.gf('django.db.models.fields.TextField')(default='', blank=True)),
-            ('redirect_url', self.gf('django.db.models.fields.URLField')(default='http://edx.org', max_length=200)),
-        ))
-        db.send_create_signal('splash', ['SplashConfig'])
+    dependencies = [
+        migrations.swappable_dependency(settings.AUTH_USER_MODEL),
+    ]
 
+    operations = [
+        migrations.CreateModel(
+            name='SplashConfig',
+            fields=[
+                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
+                ('change_date', models.DateTimeField(auto_now_add=True, verbose_name='Change date')),
+                ('enabled', models.BooleanField(default=False, verbose_name='Enabled')),
+                ('cookie_name', models.TextField(default=b'edx_splash_screen', help_text=b'The name of the cookie to check when assessing if the user needs to be redirected')),
+                ('cookie_allowed_values', models.TextField(default=b'seen', help_text=b'Comma-separated list of values accepted as cookie values to prevent the redirect')),
+                ('unaffected_usernames', models.TextField(default=b'', help_text=b'Comma-separated list of users which should never be redirected (usernames)', blank=True)),
+                ('unaffected_url_paths', models.TextField(default=b'', help_text=b'Comma-separated list of URL paths (not including the hostname) which should not be redirected. Paths may include wildcards denoted by * (example: /*/student_view)', blank=True)),
+                ('redirect_url', models.URLField(default=b'http://edx.org', help_text=b"The URL the users should be redirected to when they don't have the right cookie")),
+                ('changed_by', models.ForeignKey(on_delete=django.db.models.deletion.PROTECT, editable=False, to=settings.AUTH_USER_MODEL, null=True, verbose_name='Changed by')),
+            ],
+            options={
+                'ordering': ('-change_date',),
+                'abstract': False,
+            },
+        ),
+    ]
 
-    def backwards(self, orm):
-        # Deleting model 'SplashConfig'
-        db.delete_table('splash_splashconfig')
-
-
-    models = {
-        'auth.group': {
-            'Meta': {'object_name': 'Group'},
-            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
-            'name': ('django.db.models.fields.CharField', [], {'unique': 'True', 'max_length': '80'}),
-            'permissions': ('django.db.models.fields.related.ManyToManyField', [], {'to': "orm['auth.Permission']", 'symmetrical': 'False', 'blank': 'True'})
-        },
-        'auth.permission': {
-            'Meta': {'ordering': "('content_type__app_label', 'content_type__model', 'codename')", 'unique_together': "(('content_type', 'codename'),)", 'object_name': 'Permission'},
-            'codename': ('django.db.models.fields.CharField', [], {'max_length': '100'}),
-            'content_type': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['contenttypes.ContentType']"}),
-            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
-            'name': ('django.db.models.fields.CharField', [], {'max_length': '50'})
-        },
-        'auth.user': {
-            'Meta': {'object_name': 'User'},
-            'date_joined': ('django.db.models.fields.DateTimeField', [], {'default': 'datetime.datetime.now'}),
-            'email': ('django.db.models.fields.EmailField', [], {'max_length': '75', 'blank': 'True'}),
-            'first_name': ('django.db.models.fields.CharField', [], {'max_length': '30', 'blank': 'True'}),
-            'groups': ('django.db.models.fields.related.ManyToManyField', [], {'to': "orm['auth.Group']", 'symmetrical': 'False', 'blank': 'True'}),
-            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
-            'is_active': ('django.db.models.fields.BooleanField', [], {'default': 'True'}),
-            'is_staff': ('django.db.models.fields.BooleanField', [], {'default': 'False'}),
-            'is_superuser': ('django.db.models.fields.BooleanField', [], {'default': 'False'}),
-            'last_login': ('django.db.models.fields.DateTimeField', [], {'default': 'datetime.datetime.now'}),
-            'last_name': ('django.db.models.fields.CharField', [], {'max_length': '30', 'blank': 'True'}),
-            'password': ('django.db.models.fields.CharField', [], {'max_length': '128'}),
-            'user_permissions': ('django.db.models.fields.related.ManyToManyField', [], {'to': "orm['auth.Permission']", 'symmetrical': 'False', 'blank': 'True'}),
-            'username': ('django.db.models.fields.CharField', [], {'unique': 'True', 'max_length': '30'})
-        },
-        'contenttypes.contenttype': {
-            'Meta': {'ordering': "('name',)", 'unique_together': "(('app_label', 'model'),)", 'object_name': 'ContentType', 'db_table': "'django_content_type'"},
-            'app_label': ('django.db.models.fields.CharField', [], {'max_length': '100'}),
-            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
-            'model': ('django.db.models.fields.CharField', [], {'max_length': '100'}),
-            'name': ('django.db.models.fields.CharField', [], {'max_length': '100'})
-        },
-        'splash.splashconfig': {
-            'Meta': {'object_name': 'SplashConfig'},
-            'change_date': ('django.db.models.fields.DateTimeField', [], {'auto_now_add': 'True', 'blank': 'True'}),
-            'changed_by': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['auth.User']", 'null': 'True', 'on_delete': 'models.PROTECT'}),
-            'cookie_allowed_values': ('django.db.models.fields.TextField', [], {'default': "'seen'"}),
-            'cookie_name': ('django.db.models.fields.TextField', [], {'default': "'edx_splash_screen'"}),
-            'enabled': ('django.db.models.fields.BooleanField', [], {'default': 'False'}),
-            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
-            'redirect_url': ('django.db.models.fields.URLField', [], {'default': "'http://edx.org'", 'max_length': '200'}),
-            'unaffected_usernames': ('django.db.models.fields.TextField', [], {'default': "''", 'blank': 'True'})
-        }
-    }
-
-    complete_apps = ['splash']
