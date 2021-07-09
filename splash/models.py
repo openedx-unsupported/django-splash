87fb562c43171517122e29cc0cb399520cfc34cd
Awais Qureshi <awais.qureshi@arbisoft.com>
2021-07-09 14:48:29 +0500

    Fixing make upgrade PR.

--- splash/models.py
+++ splash/models.py
@@ -64,7 +64,7 @@ class SplashConfig(ConfigurationModel):
 
         return [url.strip() for url in self.unaffected_url_paths.split(',')]
 
-    def save(self, *args, **kwargs):  # pylint: disable=signature-differs
+    def save(self, *args, **kwargs):
         """Call `full_clean` before saving to ensure proper validation of configuration values"""
         self.full_clean()
         super().save(*args, **kwargs)
